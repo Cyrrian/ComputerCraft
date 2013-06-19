9d3774f5ebee59346ba0251227620357fcdebc1c
William Stewart <william.r.stewart@gmail.com>
2013-06-18 22:32:34 -0500

    Fixed bugs in block functions, shortened function names, added function to make a square

--- bunker.lua
+++ bunker.lua
@@ -14,8 +14,7 @@ BUTTON_SLOT= 6
 ---------------------------------------
   
 --Check if space below is Block and if not replace
-function DoBlock_Down()
-	DoDig_Down()
+function Block_Down()
         turtle.select(BLOCK_SLOT)
         if turtle.compareDown() ~= true then
                 DoDig_Down()
@@ -25,8 +24,7 @@ function DoBlock_Down()
 end
  
 --Check if space forward is Block and if not replace
-function DoBlock_Forward()
-	DoDig_Forward()
+function Block_Forward()
         turtle.select(BLOCK_SLOT)
         if turtle.compare() ~= true then
                 DoDig_Forward()
@@ -36,8 +34,7 @@ function DoBlock_Forward()
 end
  
 --Check if space above is Block and if not replace
-function DoBlock_Up()
-	DoDig_Up()
+function Block_Up()
         turtle.select(BLOCK_SLOT)
         if turtle.compareUp() ~= true then
                 DoDig_Up()
@@ -47,31 +44,31 @@ function DoBlock_Up()
 end
  
 --Dig down until space is clear
-function DoDig_Down()
+function Dig_Down()
         repeat
                 turtle.digDown()
-                sleep(0.5)
+                sleep(0.25)
         until turtle.detectDown() ~= true
 end
  
 --Dig forward until space is clear
-function DoDig_Forward()
+function Dig_Forward()
         repeat
                 turtle.dig()
-                sleep(0.5)
+                sleep(0.25)
         until turtle.detect() ~= true
 end
  
 --Dig up until space is clear
-function DoDig_Up()
+function Dig_Up()
         repeat
                 turtle.digUp()
-                sleep(0.5)
+                sleep(0.25)
         until turtle.detectUp() ~= true
 end
  
 --Check fuel level and refuel if less than 10
-function DoFuel()
+function Fuel()
         if turtle.getFuelLevel() ~= 'unlimited' then
                 if turtle.getFuelLevel() < 10 then
                         turtle.select(FUEL_SLOT)
@@ -82,140 +79,40 @@ function DoFuel()
         end
 end
  
-function DoLight()     
-                        DoDig_Up()
-                        turtle.select(LIGHT_SLOT)
-                        turtle.placeUp()
+function Square(x)
+
+	Block_Down()
+	
+	for i = 1, x-1 do
+		
+		turtle.turnLeft()
+		
+		for j=1, i do
+			turtle.forward()
+			Block_Down()
+		end
+		
+		turtle.turnLeft()
+		
+		for j=1, i do
+			turtle.forward()
+			Block_Down()
+		end
+	end
+	
+	for i = 1, x-1 do
+		
+		turtle.turnLeft()
+
+		turtle.forward()
+		Block_Down()
+
+	end
+
 end
- 
 ---------------------------------------
 --Main
 ---------------------------------------
-DoFuel()
+Fuel()
 
-
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
-
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.forward()
-DoBlock_Down()
-turtle.turnLeft()
-turtle.forward()
+Square(7)
