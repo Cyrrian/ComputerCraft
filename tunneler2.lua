a1d27851f2f24d0b6f8c4b7ac452d2e852885837
William Stewart <william.r.stewart@gmail.com>
2013-06-21 19:50:35 -0500

    reorder

--- tunneler2.lua
+++ tunneler2.lua
@@ -8,21 +8,6 @@ BLOCK_SLOT = 3
 CHEST_SLOT = 4
 
 ---------------------------------------
---Initialization
----------------------------------------
- 
-args = { ... }
-intLength = tonumber(args[1])
-intLength = intLength or 5
-if intLength % 5 ~= 0 then
-	intLength = intLength - (intLength % 5)
-end
-
-locStart = vector.new(gps.locate())
-
-intFacing = GetFacing()
-
----------------------------------------
 --Functions
 ---------------------------------------
 
@@ -45,9 +30,22 @@ function GetFacing()
 	
 end
 
+---------------------------------------
+--Initialization
+---------------------------------------
+ 
+args = { ... }
+intLength = tonumber(args[1])
+intLength = intLength or 5
+if intLength % 5 ~= 0 then
+	intLength = intLength - (intLength % 5)
+end
+
+locStart = vector.new(gps.locate())
+
+intFacing = GetFacing()
 
 ---------------------------------------
 --Main
 ---------------------------------------
-
 print("x: " .. locStart.x .. " y: " .. locStart.y .. " z: " .. z .. " f: " .. intFacing)
